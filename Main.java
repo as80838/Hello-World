9a3b35004abaa61bbd67730836a6a18df83f572e
as80838 <69618544+as80838@users.noreply.github.com>
2020-08-14 10:44:54 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Ansha");
+		System.out.println("My favorite color is black");
+		System.out.println("I like Chick-Fil-A");
+		System.out.println("I want to attend UC Irvine");
+		System.out.println("I want to major in Business Information Managment.");
+	}
+
+}
